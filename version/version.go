78d38873017064b1a42320ecad6adc3eab105678
ottodashadow <ottodashadow@users.noreply.github.com>
2021-03-09 10:01:13 -0500

    Add GitHub release.yml

---
+++ version/version.go
@@ -0,0 +1,46 @@
+package version
+
+import "strings"
+
+const version = "0.1-dev"
+
+var (
+	commit = ""
+	date   = ""
+	info   Info
+)
+
+// Info defines version details
+type Info struct {
+	Version    string `json:"version"`
+	BuildDate  string `json:"build_date"`
+	CommitHash string `json:"commit_hash"`
+}
+
+// GetAsString returns the string representation of the version
+func String() string {
+	var sb strings.Builder
+	sb.WriteString(info.Version)
+	if len(info.CommitHash) > 0 {
+		sb.WriteString("-")
+		sb.WriteString(info.CommitHash)
+	}
+	if len(info.BuildDate) > 0 {
+		sb.WriteString("-")
+		sb.WriteString(info.BuildDate)
+	}
+	return sb.String()
+}
+
+func init() {
+	info = Info{
+		Version:    version,
+		CommitHash: commit,
+		BuildDate:  date,
+	}
+}
+
+// Get returns the Info struct
+func Get() Info {
+	return info
+}
