9ee06076baff50cbf1aa256832c76886103cd6e2
ottodashadow <ottodashadow@users.noreply.github.com>
2021-03-10 11:25:20 -0500

    consul: support using a consul.json file to provide the http token

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,27 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/go-acme/lego/v4/log"
+	"github.com/hashicorp/consul/api"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/ottodashadow/lego-consul/config"
 )
 
+type consulCfg struct {
+	Token string `json:"token"`
+}
+
 var RootCmd = &cobra.Command{
 	Use: "lego-consul",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		log.Logger = logrus.StandardLogger()
 		logrus.SetFormatter(&logrus.TextFormatter{
 			FullTimestamp:   true,
@@ -22,5 +31,25 @@ var RootCmd = &cobra.Command{
 		if config.Quiet {
 			logrus.SetLevel(logrus.WarnLevel)
 		}
+
+		ConsulCfgPath := fmt.Sprintf(`%s/conf.d/consul.json`, config.Path)
+		if _, err := os.Stat(ConsulCfgPath); err == nil {
+			Data, err := ioutil.ReadFile(ConsulCfgPath)
+			if err != nil {
+				return err
+			}
+			var cfg consulCfg
+			if err := json.Unmarshal(Data, &cfg); err != nil {
+				return err
+			}
+
+			if os.Getenv(api.HTTPTokenEnvName) == `` && cfg.Token != `` {
+				if err := os.Setenv(api.HTTPTokenEnvName, cfg.Token); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
 	},
 }
